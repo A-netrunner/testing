30efba61ce76988f99d72cb55e40df9535cebf73
A-netrunner <halo37h117h@gmail.com>
2024-12-02 23:33:21 -0800

    "fix"

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,13 +1,13 @@
 const Hero = () => (
-    <section className="h-screen flex items-center justify-center relative bg-cover bg-center"
-    style={{ backgroundImage: 'url("/img2.png")' }}
->
-
+    <section
+        className="h-screen flex items-center justify-center relative bg-cover bg-center"
+        style={{ backgroundImage: 'url("/testing/img2.png")' }} 
+    >
         {/* Blur Overlay */}
         <div className="absolute inset-0 bg-black bg-opacity-80 blur-sm"></div>
 
         <div className="text-center relative z-10 text-white">
-            <h1 className="text-4xl font-bold">Hi, i&#39;m Jay </h1>
+            <h1 className="text-4xl font-bold">Hi, I&#39;m Jay</h1>
             <p className="mt-4 text-lg">Frontend Developer | React Enthusiast</p>
         </div>
     </section>
